45888cc266a512664af9db90145a980577ad38ca
Mark Shevchenko <53155116+Lucky-sketch@users.noreply.github.com>
2024-02-29 00:19:33 +0100

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -6,7 +6,7 @@ import time
 from streamlit_folium import folium_static, st_folium
 from folium.plugins import Draw
 from folium import plugins
-input_data = {}
+
 
 #Define the URL of the FastAPI endpoint
 FASTAPI_URL = 'https://immo-eliza-deployment-1-mn9i.onrender.com/predict'
@@ -235,7 +235,7 @@ def page_three():
     #Button to trigger prediction
     if st.button('Predict Price'):
         # Prepare input data as JSON
-        input_data = {
+        st.session_state.input_data = {
     "nbr_frontages": st.session_state.nbr_frontages,
     "equipped_kitchen": equipped_kitchen,
     "nbr_bedrooms": st.session_state.nbr_bedrooms,
@@ -260,7 +260,7 @@ def page_three():
 
     # Make POST request to FastAPI endpoint
     try:
-        response = requests.post(FASTAPI_URL, json=input_data)
+        response = requests.post(FASTAPI_URL, json=st.session_state.input_data)
         predicted_price = response.json()[0]
 
         progress_bar = st.progress(0)
